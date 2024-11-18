0555167b3413bf40f936cfa803cd85eb68033770
DEDpulSUPER2002 <105590890+DEDpulSUPER2002@users.noreply.github.com>
2024-11-18 12:47:28 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# print ("Hello, world!") # // - целочисленное деление, / - деление; * - умножение, ** - возведение в степень
+# int - целые числа; float - десятичные числа
+# print (type("Hello, world!")) # string - строка
+# print('Hello ' + 'world')                        # concatenate - сложение в строках
+#print((int('5')+6)) # boolean - логический тип данных
+
+
+# "1st program"
+print(9**0.5*5)
+
+#"2nd program"
+print(9.99>9.98 and 1000!=1000.1)
+
+# "3rd program"
+print(2*2+2==2*(2+2))
+
+# 4th program
+
+print(int(float("123.456")*10%10))
