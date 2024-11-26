4f6c0dc94f9eab1a45d587180f4fb62fa64fa3db
DEDpulSUPER2002 <105590890+DEDpulSUPER2002@users.noreply.github.com>
2024-11-26 14:37:46 +0300

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -1,15 +1,21 @@
-name = 'Rustam'
-print(name)
-age=22
-print(age)
-age = (age+1)
-print(age)
-is_student = True
-print(is_student)
+#name = 'Rustam'
+#print(name)
+#age=22
+#print(age)
+#age = (age+1)
+#print(age)
+#is_student = True
+#print(is_student)
 
-курс = 'Phyton'
-всего=12
-затрачено=1.5
-одна_задача = затрачено/всего
-print("Курс:",курс,", всего задач:",всего,", затрачено часов:",затрачено,", среднее время выполнения:", одна_задача,"часа" )
+#курс = 'Phyton'
+#всего=12
+#затрачено=1.5
+#одна_задача = затрачено/всего
+#print("Курс:",курс,", всего задач:",всего,", затрачено часов:",затрачено,", среднее время выполнения:", одна_задача,"часа" )
 
+example="Рустам"
+print(example[0])
+print(example[-1])
+print(example[3:])
+print(example[::-1])
+print(example[1:7:2])
